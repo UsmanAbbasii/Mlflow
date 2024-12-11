5d263c44cb487212fdf4fb0e0f1d0646c2635fdb
Usman Abbasi <f19-0831@student.uoh.edu.pk>
2024-12-11 11:30:14 +0500

    final_model with hyperparameter tuning

--- notebooks/exp3_lor_bow_hp.py
+++ notebooks/exp3_lor_bow_hp.py
@@ -16,8 +16,8 @@ import numpy as np
 import os
 import dagshub
 
-mlflow.set_tracking_uri('https://dagshub.com/campusx-official/mlops-mini-project.mlflow')
-dagshub.init(repo_owner='campusx-official', repo_name='mlops-mini-project', mlflow=True)
+mlflow.set_tracking_uri('https://dagshub.com/ussmanabbasi001/Mlflow.mlflow')
+dagshub.init(repo_owner='ussmanabbasi001', repo_name='Mlflow', mlflow=True)
 
 # Load the data
 # Load the data
